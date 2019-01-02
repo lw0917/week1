751504b8eea83cd041744be5eb672555c99e059d
LW <2955014319@qq.com>
2019-01-02 09:20:22 +0800

    js任务

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,26 @@ var server=require('gulp-webserver');
 var fs=require('fs');
 var path=require('path');
 var url=require('url');
+var mincss=require('gulp-clean-css');
 
   //编译sass
   gulp.task('css',function(){
        return gulp.src('./src/sass/*.scss')
               .pipe(sass())
               .pipe(concat('all.css'))
+              .pipe(mincss())
               .pipe(gulp.dest('./src/css/'))
   })
   //监听sass
   gulp.task('watch',function(){
       return gulp.src('./src/sass/*.scss',gulp.series('css'))
   })
- 
+    //压缩js
+    gulp.task('minJs',function(){
+        return gulp.src('./src/js/index.js')
+               .pipe(uglify())
+               .pipe(gulp.dest('./dist/js/'))
+    })
   //起服务
   gulp.task('dev',function(){
       return gulp.src('./src')
@@ -37,12 +44,8 @@ var url=require('url');
              }))
   })
 
-   gulp.task('default',gulp.series('css','dev','watch'))
+   gulp.task('default',gulp.series('css','minJs','dev','watch'))
    
 
-    //压缩js
-    gulp.task('minJs',function(){
-        return gulp.src('./src/js/index.js')
-               .pipe(uglify())
-               .pipe('./dist/js/')
-    })
+  
+  
